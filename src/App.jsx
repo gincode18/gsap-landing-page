6b11a0ea12c47630636ee0917b53836fc2fd2891
gincode18 <vishalkamboj9211@gmail.com>
2023-08-07 15:24:11 +0530

    fixed time line

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,56 @@ import { Expo } from 'gsap/gsap-core';
 
 function App() {
   const imageContainerRef = useRef(null);
+  const imagerest=()=>{
+    gsap.to(".image-container", {
+      width: "0%",
+      duration:0,
+     onComplete:animateImageContainers
+      
+    });
 
-  useEffect(() => {
-    // Animation code using gsap and React ref
+  }
+  const textrest=()=>{
+    gsap.to(".text h1", {
+      top:"110px",
+      onComplete:animateText
+    });
+
+  }
+  const animateImageContainers = () => {
     gsap.to(".image-container", {
       width: "100%",
       ease: Expo.easeInOut,
       duration: 2,
-      stagger:2
+      stagger:2,
+      delay:3,
+      onComplete: imagerest, // Call animateImageContainers again when the animation completes
     });
+  };
+  const animateText = () => {
     gsap.to(".text h1", {
       ease: Expo.easeInOut,
       stagger:2,
       top:0,
-    });
+      delay:3,
+      
+    })
     gsap.to(".text h1", {
       ease: Expo.easeInOut,
       stagger:2,
       top:"110px",
-      delay:2
+      delay:5, 
+      onComplete:animateText 
     });
+  };
+
+  useEffect(() => {
+ 
+    animateImageContainers()
+    animateText()
+   
+    
+    
   }, []);
 
   return (
@@ -34,6 +64,10 @@ function App() {
         <h1>Gear 1</h1>
         <h1>Gear 2</h1>
         <h1>Gear 3</h1>
+        <h1>Gear 4 Boundman</h1>
+        <h1>Gear 4 Tankman</h1>
+        <h1>Gear 4 Snakeman</h1>
+        <h1>Gear 5</h1>
       </div>
       <div id='one' className='image-container'>
         <img src='./gear1.jpg' alt='agf'></img>
@@ -43,6 +77,18 @@ function App() {
       </div>
       <div id='three' className='image-container'>
         <img src='./gear3.png' alt='agf'></img>
+      </div>
+      <div id='three' className='image-container'>
+        <img src='./gear4b.png' alt='agf'></img>
+      </div>
+      <div id='three' className='image-container'>
+        <img src='./gear4t.jpg' alt='agf'></img>
+      </div>
+      <div id='three' className='image-container'>
+        <img src='./gear4s.jpg' alt='agf'></img>
+      </div>
+      <div id='three' className='image-container'>
+        <img src='./gear5.jpg' alt='agf'></img>
       </div>
 
     </div>
